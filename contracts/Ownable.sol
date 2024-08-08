78b9c00c6b97f049648b5ee519158cebaeac437b
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-08-08 09:47:08 +0100

    feat: modify my ownable contract, amde it payable

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,33 +1,48 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-
+/**
+ * @title Ownable
+ * @dev This contract sets up an ownership system.
+ * It allows for transferring ownership and restricting access to certain functions to the owner only.
+ */
 contract Ownable {
+    address internal owner;
 
-    address private owner;
+    event changedOwner(address indexed newOwner, address indexed oldOwner, string message);
 
-    event ChangeOwner(address indexed oldOwner, address indexed  newOwner);
-
-    constructor(){
-        owner = msg.sender;
+    /**
+     * @dev Sets the initial owner of the contract to the address deploying the contract.
+     */
+    constructor() payable {
+        owner = payable(msg.sender);
     }
 
-
-    modifier  onlyOwner {
-        require(owner == msg.sender, "Caller not owner");
+    /**
+     * @dev Modifier to restrict function access to the contract owner only.
+     */
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You are not allowed");
         _;
     }
 
-
-    function getOwner() public  view returns (address){
+    /**
+     * @dev Retrieve the current owner of the contract.
+     * @return The address of the current owner.
+     */
+    function getOwner() internal view returns (address) {
         return owner;
     }
 
+    /**
+     * @dev Change the owner of the contract to a new address.
+     * @param _newOwner The address of the new owner.
+     * Emits a {changedOwner} event.
+     */
+    function changeOwner(address payable _newOwner) public onlyOwner {
+        require(_newOwner != address(0), "Invalid address");
 
-    function changeOwner(address _newOwner) internal onlyOwner {
-        require(_newOwner != address(0), "Owner can not be address zero");
-        
-        emit ChangeOwner(owner, _newOwner);
+        emit changedOwner(_newOwner, owner, "This owner has been changed");
         owner = _newOwner;
     }
 }
