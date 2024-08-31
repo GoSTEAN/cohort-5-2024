2ab7553d79303a40b9d263222a35fa08f0122dae
sprtd <sagesdn@gmail.com>
2024-08-31 13:13:36 +0100

    test(StudentRegistryV2): validate deployment logic

--- test/StudentRegistryV2.js
+++ test/StudentRegistryV2.js
@@ -1,27 +1,62 @@
-// const {
-//     time,
-//     loadFixture,
-//   } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-//     const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-//   const { expect } = require("chai");
-//   const { ethers } = require("hardhat");
+const {
+    time,
+    loadFixture,
+  } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+    const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+  const { expect } = require("chai");
+  const { ethers } = require("hardhat");
   
-//   describe("StudentRegistryV2 Test Suite", () => {
-//     // deploy util function
-//     const deployUtil = async () => {
-//       const StudentRegistryV2 = await ethers.getContractFactory("StudentRegistryV2"); // instance of StudentRegistryV2 contract in Contracts folder
-//       const deployedStudentRegistryV2 = await StudentRegistryV2.deploy(); // the deployed version of the StudentRegistryV2 contract in the network
-//       return deployedStudentRegistryV2; // returning the deployed StudentRegistryV2 instance
-//     };
+  describe("StudentRegistryV2 Test Suite", () => {
+    // deploy util function
+    const deployUtil = async () => {
+      const StudentRegistryV2 = await ethers.getContractFactory("StudentRegistryV2"); // instance of StudentRegistryV2 contract in Contracts folder
+      const deployedStudentRegistryV2 = await StudentRegistryV2.deploy(); // the deployed version of the StudentRegistryV2 contract in the network
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000' 
+      return {deployedStudentRegistryV2, owner, addr1, addr2, ZERO_ADDRESS}; 
+    };
   
-//     describe("Deployment", () => {
-//       it("should deploy succesfuly and return default values of count", async () => {
-//         const deployedStudentRegistryV2 = await loadFixture(deployUtil);
-  
-//         // check initial value of count
-//         let initialCountValue = await deployedStudentRegistryV2.count();
-//         expect(initialCountValue).to.eq(0); // assertion that the value of  count variable upon deployment is 0
-//       });
-//     });
-//   });
+    describe("Deployment", () => {
+      it("should deploy succesfuly and return default values of count", async () => {
+        const {deployedStudentRegistryV2, owner, ZERO_ADDRESS, addr1 } = await loadFixture(deployUtil);
+        // check the owner of the StudentRegistry contract which auto inherits from Ownable
+        let studentRegistryV2Owner = await deployedStudentRegistryV2.getOwner();
+        console.log("registry owner___", studentRegistryV2Owner)
+        expect(studentRegistryV2Owner).to.eq(owner)
+
+        // check that students array of Student struct is empty 
+        let allStudents = await deployedStudentRegistryV2.getAllStudents();
+        console.log("all students___", allStudents)
+        expect(allStudents.length).to.eq(0)
+
+        // check that studentsMapping has default values 
+        // meaning no values pushed to studentsMapping upon successful deployment
+        let studentsMapping = await deployedStudentRegistryV2.studentsMapping(ZERO_ADDRESS)
+        let addr1Mapping = await deployedStudentRegistryV2.studentsMapping(addr1)
+        console.log("student mapping___", ...studentsMapping)
+        console.log("no of items in students mapping___", studentsMapping.length)
+        const emptyStudentStruct = [
+            '0x0000000000000000000000000000000000000000',
+            '',
+  "0",
+  "0",
+  false,
+  false
+      ]
+        expect(...addr1Mapping).to.eq(...emptyStudentStruct)
+        expect(...studentsMapping).to.eq(...emptyStudentStruct)
+
+         // check that tempstudentsMapping  has default values 
+        const tempStudentsMapping = await deployedStudentRegistryV2.tempstudentsMapping(addr1)
+        expect(...tempStudentsMapping).to.eq(...emptyStudentStruct)
+   
+        // check if  hasPaidMapping has default value of false
+        const hasPaidMapping = await deployedStudentRegistryV2.hasPaidMapping(addr1)
+        expect(hasPaidMapping).to.eq(false)
+        
+
+        // expect(initialCountValue).to.eq(0); // assertion that the value of  count variable upon deployment is 0
+      });
+    });
+  });
   
