ae24de01095247a580166babeefa4b835608170f
sprtd <sagesdn@gmail.com>
2024-09-01 18:17:13 +0100

    chore: remove console.logs

--- test/StudentRegistryV2.js
+++ test/StudentRegistryV2.js
@@ -35,20 +35,16 @@ describe("StudentRegistryV2 Test Suite", () => {
       const { deployedStudentRegistryV2, owner, ZERO_ADDRESS, addr1 } = await loadFixture(deployUtil);
       // check the owner of the StudentRegistry contract which auto inherits from Ownable
       let studentRegistryV2Owner = await deployedStudentRegistryV2.getOwner();
-      console.log("registry owner___", studentRegistryV2Owner);
       expect(studentRegistryV2Owner).to.eq(owner);
 
       // check that students array of Student struct is empty
       let allStudents = await deployedStudentRegistryV2.getAllStudents();
-      console.log("all students___", allStudents);
       expect(allStudents.length).to.eq(0);
 
       // check that studentsMapping has default values
       // meaning no values pushed to studentsMapping upon successful deployment
       let studentsMapping = await deployedStudentRegistryV2.studentsMapping(ZERO_ADDRESS);
       let addr1Mapping = await deployedStudentRegistryV2.studentsMapping(addr1);
-      console.log("student mapping___", ...studentsMapping);
-      console.log("no of items in students mapping___", studentsMapping.length);
       const emptyStudentStruct = ["0x0000000000000000000000000000000000000000", "", "0", "0", false, false];
       expect(...addr1Mapping).to.eq(...emptyStudentStruct);
       expect(...studentsMapping).to.eq(...emptyStudentStruct);
@@ -72,8 +68,6 @@ describe("StudentRegistryV2 Test Suite", () => {
             deployedStudentRegistryV2.connect(owner).payFee({ value: convertEther("1") })
           ).to.be.revertedWith("Owner is excluded");
 
-          console.log("Deployed contract address:", deployedStudentRegistryV2.address);
-          console.log(deployedStudentRegistryV2);
         });
 
         it("should revert attempt to proceed without paying 1ETH as fee", async () => {
@@ -92,12 +86,9 @@ describe("StudentRegistryV2 Test Suite", () => {
 
           // Fetch contract balance before payFee txn
           const initialContractBalance = await getBalance(deployedStudentRegistryV2Address);
-          console.log("Contract's initial balance:", ethers.formatEther(initialContractBalance));
 
           // Fetch addr1  balance of  before payFee txn
           const initialPayerBalance = await getBalance(addr1.address);
-
-          console.log("Payer's initial balance:", ethers.formatEther(initialPayerBalance));
 
           // Perform the payment
           const tx = await deployedStudentRegistryV2.connect(addr1).payFee({ value: ethers.parseEther("1") });
@@ -106,9 +97,6 @@ describe("StudentRegistryV2 Test Suite", () => {
           // Fetch updated balances
           const finalPayerBalance = await getBalance(addr1.address);
           const finalContractBalance = await getBalance(deployedStudentRegistryV2Address);
-
-          console.log("Payer's final balance:", ethers.formatEther(finalPayerBalance));
-          console.log("Contract's final balance:", ethers.formatEther(finalContractBalance));
 
           // Assert the balance changes
           const initialPayerBalanceNum = parseFloat(ethers.formatEther(initialPayerBalance));
@@ -131,29 +119,6 @@ describe("StudentRegistryV2 Test Suite", () => {
             .to.emit(deployedStudentRegistryV2, "PaidFee")
             .withArgs(addr1.address, ethers.parseEther("1"));
         });
-      });
-    });
-    describe("RegisterStudent Transaction", () => {
-      it.only("should successfully register a student", async function () {
-        const { deployedStudentRegistryV2, addr1, addr2, deployedStudentRegistryV2Address, owner } = await loadFixture(
-          deployUtil
-        );
-
-        // Make a payment first
-        await deployedStudentRegistryV2.connect(addr1).payFee({ value: ethers.parseEther("1") });
-
-        // Perform registration
-        await expect(deployedStudentRegistryV2.connect(addr1).register(addr2.address, "Alice", 20))
-          .to.emit(deployedStudentRegistryV2, "registerStudent")
-          .withArgs(addr2.address, "Alice", 20);
-
-        // Verify the student was added
-        const student = await deployedStudentRegistryV2.tempstudentsMapping(addr2.address);
-        expect(student.studentAddr).to.equal(addr2.address);
-        expect(student.name).to.equal("Alice");
-        expect(student.age).to.equal(20);
-        expect(student.hasPaid).to.be.true;
-        expect(student.isAuthorized).to.be.false;
       });
     });
   });
